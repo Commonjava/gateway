f5e1a6e97dfc8906cff370efc1b9b06de4ce3475
Rui Han <ruhan@redhat.com>
2020-11-13 15:14:48 +0800

    Support get bytes

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# gateway
-API gateway.
+# Gateway
+API gateway works as a facade to hide micro services from end users.
+Under the hook, it receives HTTP requests from client (e.g., mvn) and
+forward to different services.
+
+There is a classifier which determines the target service via URL
+patterns. For example, "/api/content" requests go to indy and
+"/api/promote" requests go to promotion.
+
+## Prerequisite
+1. jdk11
+2. mvn 3.6.2+
+
+## Try it!
+$ mvn compile quarkus:dev
+
+This will start Gateway in debug mode. Open another terminal and try
+some maven build.
+
+E.g,
+mvn clean install -DskipTests -s <path-to>/settings-gateway.xml
+
+The settings-gateway.xml is at the project root directory for test
+purpose.
+
+## TODO:
+
+It only supports GET. PUT and POST is yet to come.
--- src/main/java/org/commonjava/util/gateway/services/ProxyService.java
+++ src/main/java/org/commonjava/util/gateway/services/ProxyService.java
@@ -1,48 +1,46 @@
 package org.commonjava.util.gateway.services;
 
 import io.smallrye.mutiny.Uni;
-import io.vertx.core.MultiMap;
 import io.vertx.core.http.HttpServerRequest;
-import io.vertx.core.json.JsonObject;
-import io.vertx.mutiny.core.buffer.Buffer;
-import io.vertx.mutiny.ext.web.client.HttpResponse;
 import io.vertx.mutiny.ext.web.client.WebClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
 
 @ApplicationScoped
 public class ProxyService
 {
     private final Logger logger = LoggerFactory.getLogger( getClass() );
 
-    public Uni<String> doRequest( WebClient client, String path, HttpServerRequest request )
+    @Inject
+    private Classifier classifier;
+
+    public Uni<byte[]> doRequestBytes( String path, HttpServerRequest request )
     {
-        return client.get( path ).putHeaders( getHeaders( request ) ).send().onItem().transform( resp -> {
-            logger.debug("Get resp: {}", resp.statusMessage() );
+        WebClient client = classifier.getWebClient( path );
+
+        return client.get( path )./*putHeaders( getHeaders( request ) ).*/send().onItem().transform( resp -> {
+            logger.debug( "Get resp: {}, headers: {}", resp.statusMessage(), resp.headers() );
             if ( resp.statusCode() == 200 )
             {
-                return resp.bodyAsString();
+                return resp.body().getBytes();
             }
             else
             {
-                return getErrorMessage( resp );
+                return null;
             }
         } );
     }
 
-
-    private String getErrorMessage( HttpResponse<Buffer> resp )
-    {
-        return new JsonObject().put( "code", resp.statusCode() ).put( "message", resp.bodyAsString() ).toString();
-    }
-
+/*
     private io.vertx.mutiny.core.MultiMap getHeaders( HttpServerRequest request )
     {
         MultiMap headers = request.headers();
         //headers.names().forEach( name -> System.out.println( ">>> " + name + ": " + headers.getAll( name ) ) );
         return io.vertx.mutiny.core.MultiMap.newInstance( headers ).remove( "Host" );
     }
+*/
 
 }
