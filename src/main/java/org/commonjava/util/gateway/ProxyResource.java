71091e4598f037bd10b3ac08d108e6443bfab53e
Rui Han <ruhan@redhat.com>
2020-11-13 15:40:59 +0800

    Support getbytes (#3)
    * Support get bytes

--- src/main/java/org/commonjava/util/gateway/ProxyResource.java
+++ src/main/java/org/commonjava/util/gateway/ProxyResource.java
@@ -2,42 +2,31 @@ package org.commonjava.util.gateway;
 
 import io.smallrye.mutiny.Uni;
 import io.vertx.core.http.HttpServerRequest;
-import io.vertx.mutiny.ext.web.client.WebClient;
-import org.commonjava.util.gateway.services.Classifier;
 import org.commonjava.util.gateway.services.ProxyService;
 import org.jboss.resteasy.annotations.jaxrs.PathParam;
-import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
-import javax.ws.rs.core.MediaType;
 
 @Path( "/api" )
 public class ProxyResource
 {
-    private final Logger logger = LoggerFactory.getLogger( getClass() );
+    private final org.slf4j.Logger logger = LoggerFactory.getLogger( getClass() );
 
     @Inject
     private ProxyService proxyService;
 
-    @Inject
-    private Classifier classifier;
-
-    // TODO: GET / PUT / POST via bytes stream
+    // TODO: PUT / POST via bytes / stream
 
     @GET
-    @Produces( MediaType.APPLICATION_XML )
     @Path( "/{path: (.*)}" )
-    public Uni<String> getData( @PathParam( "path" ) String path, final @Context HttpServerRequest request )
+    public Uni<byte[]> getBytes( @PathParam( "path" ) String path, final @Context HttpServerRequest request )
     {
-        WebClient client = classifier.getWebClient( path );
-
         logger.debug( "Get resource: {}", path );
-        return proxyService.doRequest( client, "/api/" + path, request );
+        return proxyService.doRequestBytes( "/api/" + path, request );
     }
 
 }
--- src/test/java/org/commonjava/util/gateway/ProxyResourceTest.java
+++ src/test/java/org/commonjava/util/gateway/ProxyResourceTest.java
@@ -1,12 +1,12 @@
 package org.commonjava.util.gateway;
 
 import io.quarkus.test.junit.QuarkusTest;
-import io.smallrye.mutiny.Uni;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.notNullValue;
 
 @QuarkusTest
 public class ProxyResourceTest
@@ -19,6 +19,16 @@ public class ProxyResourceTest
                .then()
                .statusCode( 200 )
                .body( containsString( "<artifactId>partyline</artifactId>" ) );
+    }
+
+    @Test
+    public void testProxyBytesAsyncEndpoint()
+    {
+        given().when()
+               .get( "/api/content/maven/hosted/local-deployments/org/commonjava/util/partyline/2.1-SNAPSHOT/partyline-2.1-20191014.214930-1.jar" )
+               .then()
+               .statusCode( 200 )
+               .body( is( notNullValue() ) );
     }
 
 }
