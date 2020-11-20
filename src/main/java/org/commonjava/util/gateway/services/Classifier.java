26db757e2240fff07d696742021a43a28f9b82c8
Rui Han <ruhan@redhat.com>
2020-11-20 10:59:03 +0800

    Support log to file and print config

--- src/main/java/org/commonjava/util/gateway/services/Classifier.java
+++ src/main/java/org/commonjava/util/gateway/services/Classifier.java
@@ -1,5 +1,6 @@
 package org.commonjava.util.gateway.services;
 
+import io.quarkus.runtime.Startup;
 import io.vertx.core.http.HttpMethod;
 import io.vertx.core.http.HttpServerRequest;
 import io.vertx.ext.web.client.WebClientOptions;
@@ -10,12 +11,14 @@ import org.commonjava.util.gateway.exception.ServiceNotFoundException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Function;
 
+@Startup
 @ApplicationScoped
 public class Classifier
 {
@@ -26,6 +29,14 @@ public class Classifier
 
     @Inject
     private ProxyServiceConfiguration serviceConfiguration;
+
+    @PostConstruct
+    void init()
+    {
+        serviceConfiguration.services.forEach(
+                        service -> logger.info( "Proxy service, host: {}, port: {}, methods: {}, pathPattern: {}",
+                                                service.host, service.port, service.methods, service.pathPattern ) );
+    }
 
     private Map<ProxyServiceConfiguration.ServiceConfig, WebClient> clientMap = new ConcurrentHashMap<>();
 
@@ -41,7 +52,7 @@ public class Classifier
         ProxyServiceConfiguration.ServiceConfig service = null;
         for ( ProxyServiceConfiguration.ServiceConfig sv : serviceConfiguration.services )
         {
-            if ( path.matches( sv.pathPattern ) && sv.methods.contains( method.name() ) )
+            if ( path.matches( sv.pathPattern ) && ( sv.methods == null || sv.methods.contains( method.name() ) ) )
             {
                 service = sv;
                 break;
