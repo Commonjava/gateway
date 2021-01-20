1022386b2e96ff2de8f13fbc9be9485b10ebb191
Rui Han <ruhan@redhat.com>
2021-01-20 11:44:12 +0800

    Add health checks

---
+++ src/main/java/org/commonjava/util/gateway/metrics/health/ThreadsHealthCheck.java
@@ -0,0 +1,24 @@
+package org.commonjava.util.gateway.metrics.health;
+
+import org.commonjava.util.gateway.metrics.honeycomb.JvmRootSpanFields;
+import org.eclipse.microprofile.health.HealthCheck;
+import org.eclipse.microprofile.health.HealthCheckResponse;
+import org.eclipse.microprofile.health.Liveness;
+
+import javax.enterprise.context.ApplicationScoped;
+import java.util.Optional;
+
+import static org.eclipse.microprofile.health.HealthCheckResponse.State.UP;
+
+@Liveness
+@ApplicationScoped
+public class ThreadsHealthCheck
+                implements HealthCheck
+{
+    @Override
+    public HealthCheckResponse call()
+    {
+        return new HealthCheckResponse( "threads", UP,
+                                        Optional.of( JvmRootSpanFields.getInstance().getThreadsInfo() ) );
+    }
+}
